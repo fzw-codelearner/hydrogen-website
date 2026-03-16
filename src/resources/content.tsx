304f5fb6462a70ae3ae74c30342d02989f11b373
longwait <3498363975@qq.com>
2026-03-16 20:50:53 +0800

    Basic Init

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -2,14 +2,13 @@ import { About, Blog, Gallery, Home, Newsletter, Person, Social, Work } from "@/
 import { Line, Row, Text } from "@once-ui-system/core";
 
 const person: Person = {
-  firstName: "Selene",
-  lastName: "Yu",
-  name: `Selene Yu`,
-  role: "Design Engineer",
+  firstName: "付",
+  lastName: "泽文",
+  name: `付泽文`,
+  role: "团队混子",
   avatar: "/images/avatar.jpg",
-  email: "example@gmail.com",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  email: "3498363975@qq.com",
+  languages: ["English", "Chinese"], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter: Newsletter = {
@@ -60,15 +59,15 @@ const home: Home = {
   label: "Home",
   title: `${person.name}'s Portfolio`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
-  headline: <>Building bridges between design and code</>,
+  headline: <>Hello! I think you are so cute!</>,
   featured: {
     display: true,
     title: (
       <Row gap="12" vertical="center">
-        <strong className="ml-4">Once UI</strong>{" "}
+        <strong className="ml-4">Hydrogen Team</strong>{" "}
         <Line background="brand-alpha-strong" vert height="20" />
         <Text marginRight="4" onBackground="brand-medium">
-          Featured work
+          付泽文
         </Text>
       </Row>
     ),
@@ -76,7 +75,7 @@ const home: Home = {
   },
   subline: (
     <>
-    I'm Selene, a design engineer at <Text as="span" size="xl" weight="strong">ONCE UI</Text>, where I craft intuitive <br /> user experiences. After hours, I build my own projects.
+    这是可以是我们的团队展示网站 <Text as="span" size="xl" weight="strong">Hydrogen Team</Text>，你们看看这样行不行？ <br /> 我还可以修改一些别的东西或者换个风格？
 </>
   ),
 };
